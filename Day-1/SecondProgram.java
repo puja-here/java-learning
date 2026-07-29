8d5f9f8e251aae7f75afe258af01a13be21e1895
puja-here <pmpujamandal38@gmail.com>
2026-07-29 16:03:05 +0530

    modified SecondProgram.java by adding run command using text editor

--- Day-1/SecondProgram.java
+++ Day-1/SecondProgram.java
@@ -3,4 +3,9 @@ public class SecondProgram{
     public static void main(String[]args) {
         System.out.println("Good Morning");
     }
+
+    // create a java program to output "good morning" using text editor/notepad.
+    // first created a file with .java extension ,then write the program and run it using this command
+    // 1. "javac FileName.java"
+    // 2. "java FileName"
 }
